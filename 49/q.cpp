c84d975a17bff583320345f598f0b2c633ee6869
plantman <Hezik19800388562@outlook.com>
2026-05-18 16:31:42 +0800

    暴力解法

---
+++ 49/q.cpp
@@ -0,0 +1,44 @@
+#include<vector>
+#include<string>
+#include<map>
+#include<iostream>
+#include<algorithm>
+
+
+using namespace std;
+
+class Solution {
+public:
+    static bool cmp (char a, char b){
+        return (a<b);
+    }
+
+    static string get_key(string str) {
+        std::sort(str.begin(), str.end(), cmp);
+        return str;
+    }
+
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // if (strs[0] == "") return vector<vector<string>>(1, vector<string>(1, ""));
+        
+        vector<string> keys;
+        map<string, vector<string>> str_map;
+        for (int i=0;i<strs.size();i++) {
+            string stri = strs[i];
+            string str_key = get_key(stri);
+            if (!str_map[str_key].size()) {
+                keys.push_back(str_key);
+            }
+            str_map[str_key].push_back(stri);
+        }
+
+        vector<vector<string>> results(keys.size());
+        for (int i=0;i<keys.size();i++) {
+            for (int j=0;j<str_map[keys[i]].size();j++) {
+                results[i].push_back(str_map[keys[i]][j]);
+            }
+        }
+
+        return results;
+    }
+};
