81768b7fbeb488731f5375b44037a5825efcd958
plantman <Hezik19800388562@outlook.com>
2026-05-26 14:31:52 +0800

    暴力解法，预计超时

--- 35-三数之和/q.cpp
+++ 35-三数之和/q.cpp
@@ -7,11 +7,16 @@ using namespace std;
 
 class Solution {
 public:
+    typedef struct tuple{
+        int i,ldx,rdx;
+        tuple(int a1, int b1, int c1): i(a1), ldx(b1), rdx(c1) {}
+    };
+
     vector<vector<int>> threeSum(vector<int>& nums) {
         vector<vector<int>> results;
-        unordered_set<int> num_set;  // 保存已有的数字
+        vector<tuple> num_tups;
         sort(nums.begin(), nums.end());
-        int findz = 0;
+
         for (int i=0;i<nums.size()-2;i++) {
             if (i>0) {if (nums[i] == nums[i-1]) continue;}
             int ldx = i+1, rdx = nums.size()-1, opt=0;
@@ -19,25 +24,18 @@ public:
             while(ldx < rdx) {
                 int numsum = nums[ldx] + nums[rdx];
                 if (numsum == tarnum) {
-                    if (!(num_set.find(nums[ldx])!=num_set.end() && num_set.find(nums[rdx])!=num_set.end() && num_set.find(nums[i])!=num_set.end())){
-                        num_set.insert(nums[ldx]);
-                        num_set.insert(nums[rdx]);
-                        num_set.insert(nums[i]);
+                    tuple new_tup(nums[i], nums[ldx], nums[rdx]);
+                    int in_tups = 0;
+                    for (int j=0;j<num_tups.size();j++) {
+                        if (num_tups[j].i == nums[i] && num_tups[j].ldx == nums[ldx]) in_tups = 1;
+                    }
+                    if (!in_tups){
                         vector<int> result;
                         result.push_back(nums[i]);
                         result.push_back(nums[ldx]);
                         result.push_back(nums[rdx]);
                         results.push_back(result);
-                        if (nums[i] == 0 && nums[ldx] == 0 && nums[rdx] == 0) findz = 1;
-                    }
-
-                    else if (nums[ldx] == 0 && nums[rdx] == 0 && nums[i] == 0 && findz == 0) {
-                        vector<int> result;
-                        result.push_back(0);
-                        result.push_back(0);
-                        result.push_back(0);
-                        results.push_back(result);
-                        findz = 1;
+                        num_tups.push_back(new_tup);
                     }
                     rdx -= 1;
                 }
@@ -51,8 +49,9 @@ public:
                 }
             }
         }
-        for (auto i=num_set.begin();i!=num_set.end();i++) {
-            cout << (*i) << endl;
+
+        for (auto i=0;i<num_tups.size();i++) {
+            cout << num_tups[i].i << " " << num_tups[i].ldx << "" << num_tups[i].rdx << endl;
         }
         return results;
     }
---
+++ 42-接雨水/q.cpp
@@ -0,0 +1,51 @@
+#include<vector>
+#include<iostream>
+using namespace std;
+
+
+class Solution {
+public:
+    static int min2(int a, int b) {
+        if (a < b) return a;
+        return b;
+    }
+    static void print_vec(vector<int> heights) {
+        for (int i=0;i<heights.size();i++) {cout << heights[i] << " ";}
+        cout << endl;
+    }
+    int trap(vector<int>& heights) {
+        int rains = 0;
+        int new_place = 1;
+        while (new_place) {
+            new_place = 0;
+            for (int i=1;i<heights.size()-1;i++) {
+                if (heights[i] < heights[i-1] && heights[i] < heights[i+1]) {
+                    rains += (min2(heights[i-1], heights[i+1]) - heights[i]);
+                    heights[i] = min2(heights[i-1], heights[i+1]);
+                    new_place = 1;
+                }
+
+                if (heights[i]==heights[i+1] && heights[i-1] > heights[i]) {
+                    int j, shortage=0;
+                    for (j=i+1;j<heights.size();j++) {
+                        if (heights[j] != heights[j-1]) {
+                            if (heights[j] < heights[j-1]) {i=j;}
+                            else shortage = min2(heights[i-1], heights[j]);
+                            break;
+                        }
+                    }
+
+                    if (shortage) {
+                        for (int t=i;t<j;t++) {
+                            rains += (shortage-heights[t]);
+                            heights[t] = shortage;
+                        }
+                        new_place = 1;
+                    }
+                }
+            }
+            print_vec(heights);
+        }
+        return rains;
+    }
+};
